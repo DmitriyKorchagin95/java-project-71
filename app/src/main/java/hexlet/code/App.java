41a5493a261dfdab40c6fff7aa0e097d636ad6e2
Dmitriy Korchagin <dmitriy.korchagin95@icloud.com>
2025-10-08 23:58:56 +0300

    Integrate picocli and add CLI structure to App
    Added picocli dependency to build.gradle.kts and refactored App.java to use picocli for command-line parsing. The application now supports options and parameters for comparing configuration files, laying the groundwork for future CLI functionality.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,45 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format",
+            description = "output format [default: stylish]")
+    private String format;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "Path to first file.")
+    private String filepPath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "Path to second file.")
+    private String filepPath2;
+
+    @Override
+    public Integer call() throws Exception {
+//        var diff = Differ.generate(filePath1, filePath2);
+//        System.out.println(diff);
+        return 0;
+    }
+
+    public static void main(String... args) {
+
+        var colorScheme = new CommandLine.Help.ColorScheme.Builder(CommandLine.Help.Ansi.ON)
+                .options(CommandLine.Help.Ansi.Style.fg_red)
+                .parameters()
+                .applySystemProperties()
+                .build();
+
+        int exitCode = new CommandLine(new App())
+                .setColorScheme(colorScheme)
+                .execute(args);
+
+        System.exit(exitCode);
     }
 }
